57269b4a10a6e3d83a4efc7252ad5bcade4448f1
alexlurvey <alex.lurvey@gmail.com>
2020-05-23 09:40:58 -0500

    fix: update build for deployment

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -14,7 +14,7 @@ export const ArtistCard = ({ artist, key }: IArtistCardProps) => {
             <div>{formatNumber(artist.listeners)} listeners</div>
             <div className='artist-card-icon'>
                 <a href={artist.url} target='_blank'>
-                    <img src='assets/lastfm-icon.png' />
+                    <img src='lastfm-icon.png' />
                 </a>
             </div>
         </div>
